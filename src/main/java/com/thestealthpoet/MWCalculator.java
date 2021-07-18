159d7e0934a19d6466247bfe82237880bc3d0876
thestealthpoet <bryan.r.mclain@gmail.com>
2021-07-18 00:09:10 -0400

    getting a string to string[] to double[] to. . .still working on it

--- src/main/java/com/thestealthpoet/MWCalculator.java
+++ src/main/java/com/thestealthpoet/MWCalculator.java
@@ -4,30 +4,29 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class MWCalculator {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        String csvFilePath = "C:\\Users\\bryan\\Bryan Coding Workspace\\Molecular Weight Calculator\\ElementSymbolWeight.csv";
-        Map<String, String> elementWeightMap = new HashMap<>();
-        BufferedReader csvReader = new BufferedReader(new FileReader(csvFilePath));
+        String csvFile = "ElementSymbolWeight.csv";
+        Map<String, String> elementWeightMap = new TreeMap<>();
+        BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
         String line = "";
         while((line = csvReader.readLine()) != null) {
-            String csvline[] = line.split("/t");
-            System.out.println(csvline[0]);
+            String[] csvLine = line.split(",");
+            String weightsAsString = csvLine[1];
+            System.out.println(weightsAsString);
+            //Double[] weightsAsDouble = Arrays.stream(csvLine[1]).map(Double::valueOf).toArray(Double[]::new);
+            //elementWeightMap.put(csvLine[0], csvLine[1]);
+
+        } //System.out.println(elementWeightMap);
+        //for (Map.Entry<String, String> elementSymbol : elementWeightMap.entrySet()) {
+           // elementWeightMap.put(elementSymbol.getKey(), new Double (elementSymbol.getValue()));
         }
 
-            /*for (int i = 1; i < csvline.length-1; i++) {
-                String[] csvarray = csvline[i].split(":");
-                elementWeightMap.put(csvarray[0], csvarray[1]);
-
-                }System.out.println(elementWeightMap);
-            }*/
 
         }
 
-    }
+
 
